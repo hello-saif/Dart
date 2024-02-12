5cf5245ab1aa6253f769b736a9eb9432909c9d70
Saifuddin-Nobab <101374729+hello-saif@users.noreply.github.com>
2024-02-12 19:59:09 +0600

    Add files via upload

---
+++ main 1.dart
@@ -0,0 +1,23 @@
+void main()
+{
+  //Variables
+  //<datatype> <variables>=value;
+ //int double 
+ double function=30;
+ int function1=1000;
+ bool function2=false ;
+ dynamic function3=10.3;
+ String function4 = 'hello';
+  //print(function);
+  //print(function1);
+
+
+  //first value 
+  print(function+function1+10.22);
+  print(function2);
+  print (function1.isEven);
+  print(function3); 
+  //string 
+  print(function4.codeUnits);
+
+}
