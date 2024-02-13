99ebf129e1239ba5299db2d1daac992902174c36
Saifuddin-Nobab <101374729+hello-saif@users.noreply.github.com>
2024-02-13 13:43:02 +0600

    Center The Table
    Problem Statement
    You are working on designing a responsive mobile screen that contains a table displaying some information. You will be given the screen width W as input. Write a program to assist in calculating the table's position based on the screen width W.
    
    You are given the following information:
    
    The table's width  TW is fixed at 300 pixels.
    Table should be center with respect to the width of the sceen
    In other words the distance between the table and the left edge of the screen should be:
    Half of the difference between the screen width and table width Output the horizontal position of the table.
    
    Input
    The input consists of a single line:
    An integer W representing the width of the screen.
    
    Output
    An integer representing the left margin for the cards within the container.
    
    Constraints
    
    480 ≤ W ≤ 2160
    W will always be divisible by 10
    
    Example 1:
    Input:
    500
    Output:
    100
    Example 2:
    Input:
    1080
    Output:
    390

---
+++ CenterTheTable.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main() {
+  int screenWidth = int.parse(stdin.readLineSync()!);
+  int tableWidth = 300;
+
+  int leftMargin = (screenWidth - tableWidth) ~/ 2;
+  print(leftMargin);
+}
