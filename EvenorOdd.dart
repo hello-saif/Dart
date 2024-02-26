9b892ec94da6d3df222686017ba7f0b1f8f11d9e
Saifuddin-Nobab <101374729+hello-saif@users.noreply.github.com>
2024-02-26 22:46:42 +0600

    Update EvenorOdd.dart
    Problem Statement
    Write a program that checks if a number entered by the user is even or odd.
    
    Input
    The input consists of a integer.
    
    Output
    Output a single line explaining whether it is even or odd.
    
    Constraints
    -2 31 ≤ |S| ≤ 231 - 1
    Example-1:
    Enter a number
    
    Input:
    7
    Output:
    7 is an odd number.
    Example-2:
    Enter a number
    
    Input:
    8
    Output:
    8 is an even number.

--- EvenorOdd.dart
+++ EvenorOdd.dart
@@ -2,7 +2,9 @@ import 'dart:io';
 
 void main() {
   int number = int.parse(stdin.readLineSync()!);
-
+  EvenOrOdd(number);
+}
+void EvenOrOdd(int number) {
   if (number % 2 == 0) {
     print('$number is an even number.');
   } else {
