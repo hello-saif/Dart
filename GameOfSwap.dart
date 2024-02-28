a67d3a8f254dca394547c5c6289f7f361caf941e
Saifuddin-Nobab <101374729+hello-saif@users.noreply.github.com>
2024-02-28 12:13:09 +0600

    Game Of  Swap
    Problem Statement
    Write a program that swaps the values of two variables.
    
    Input
    The input consists of two integers.
    
    Output
    Output consists of two integers.
    
    Constraints
    -2 31 ≤ |S| ≤ 231 - 1
    Example:
    Enter two numbers
    
    Input:
    10 20
    Output:
    Before swapping: num1 = 10, num2 = 20
    After swapping: num1 = 20, num2 = 10

---
+++ GameOfSwap.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main() {
+  List<String> input = stdin.readLineSync()!.split(' ');
+  int num1 = int.parse(input[0]);
+  int num2 = int.parse(input[1]);
+
+  print("Before swapping: num1 = $num1, num2 = $num2");
+  swapping1(num1, num2);
+}
+
+void swapping1(int num1, num2) {
+  int temp = num1;
+  num1 = num2;
+  num2 = temp;
+  print("After swapping: num1 = $num1, num2 = $num2");
+}
