bee240775e817842db6006944b7e99f9e370e67d
Saifuddin-Nobab <101374729+hello-saif@users.noreply.github.com>
2024-04-20 07:59:35 +0600

    Last Occurrence
    Last Occurrence
    
    Problem Statement
    Write a program where you will be given a sorted array of integers and a target value.There will be repeated elements. Find the index of the last occurrence of the target in the array using binary search.
    
    Input
    The program will take an integer  N as the size of a sorted array. Then it will take the integer values of the array  M[]. Finally, it will take the target value  P
    
    Output
    The output will print either the index of the last occurrence of the target element or "Element not found" if the element is not found.
    
    Constraints
    0 ≤ |N| ≤ 100000
    0 ≤ |M[]| ≤ 100000
    0 ≤ |P| ≤ 100000
    Example-1:
    Enter numbers
    
    Input:
    9
    1 2 2 4 4 4 5 6 7
    4
    Output:
    5
    Example-2:
    Enter numbers
    
    Input:
    9
    1 2 2 4 4 4 5 6 7
    8
    Output:
    Element not found

---
+++ LastOccurrence.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+int binarySearchLastOccurrence(List<int> arr, int target) {
+  int low = 0;
+  int high = arr.length - 1;
+  int result = -1;
+
+  while (low <= high) {
+    int mid = low + ((high - low) ~/ 2);
+
+    if (arr[mid] == target) {
+      result = mid;
+      low = mid + 1;
+    } else if (arr[mid] < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+
+  return result;
+}
+
+void main() {
+  var n = int.parse(stdin.readLineSync()!);
+  var inputArray = stdin.readLineSync()!.split(' ').map(int.parse).toList();
+  var target = int.parse(stdin.readLineSync()!);
+
+  int result = binarySearchLastOccurrence(inputArray, target);
+
+  if (result != -1) {
+    print(result);
+  } else {
+    print("Element not found");
+  }
+}
