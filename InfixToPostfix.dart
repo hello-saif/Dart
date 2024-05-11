891998a253c7e8d1f35a6d97bfef128976a7c95b
Saifuddin-Nobab <101374729+hello-saif@users.noreply.github.com>
2024-05-11 11:51:02 +0600

    Infix to Postfix Converter
    Problem Statement
    Write a program to convert infix expression into postfix expression. Infix Expression: Operator will be between operands.e.g. 1+2 Postfix Expression: Operator will be after operands.e.g. 12+
    
    Input
    The program will take a string  S as input.
    
    Output
    The output will print a string consists of operators and operands.
    
    Constraints
    0 ≤ |S| ≤ 10000
    Only characters from 0-9 will be used
    / will be there as opertor along with ( ) in input.
    Example:
    Enter string
    
    Input:
    (8+2)-3
    Output:
    82+3-

---
+++ InfixToPostfix.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+bool isOperator(String c) {
+  return ['+', '-', '*', '/', '^'].contains(c);
+}
+
+bool isOperand(String c) {
+  return RegExp(r"^[0-9]$").hasMatch(c);
+}
+
+int precedence(String op) {
+  if (op == '+' || op == '-') {
+    return 1;
+  }
+  if (op == '*' || op == '/') {
+    return 2;
+  }
+  if (op == '^') {
+    return 3;
+  }
+  return 0;
+}
+
+String infixToPostfix(String expression) {
+  List<String> stack = [];
+  String postfix = '';
+  for (var char in expression.runes) {
+    String c = String.fromCharCode(char);
+    if (c == '(') {
+      stack.add(c);
+    } else if (c == ')') {
+      while (stack.isNotEmpty && stack.last != '(') {
+        postfix += stack.removeLast();
+      }
+      stack.removeLast(); // pop '('
+    } else if (isOperator(c)) {
+      while (stack.isNotEmpty &&
+          precedence(stack.last) >= precedence(c) &&
+          stack.last != '(') {
+        postfix += stack.removeLast();
+      }
+      stack.add(c);
+    } else if (isOperand(c)) {
+      postfix += c;
+    }
+  }
+  while (stack.isNotEmpty) {
+    postfix += stack.removeLast();
+  }
+  return postfix;
+}
+
+void main() {
+  String infixExpression = stdin.readLineSync()!;
+  String postfixExpression = infixToPostfix(infixExpression);
+  print("$postfixExpression");
+}
